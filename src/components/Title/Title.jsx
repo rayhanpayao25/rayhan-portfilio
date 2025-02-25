057931cc680b2b8e15657bcb63c67a52ed323356
Your Name <rayhanpyo2016.com>
2025-02-25 11:26:31 +0800

    new update

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import'./Title.css'
+import "./Title.css"
 
-const Title = ({subTitle ,title}) => {
+const Title = ({ subTitle, title }) => {
   return (
-    
     <div className="title">
-        <p>{subTitle}</p>
-        <h2>{title}</h2>
+      <p>{subTitle}</p>
+      <h2>{title}</h2>
     </div>
   )
 }
 
 export default Title
+
